ce6587d5336a0fe237a89bfa790341bb95c10369
ThisGuh <jakiepolska1111@gmail.com>
2022-07-07 10:50:43 +0200

    feat: change hero on index page

--- src/components/Hero/Hero.style.tsx
+++ src/components/Hero/Hero.style.tsx
@@ -2,7 +2,13 @@ import styled from 'styled-components';
 import { HEADER_HEIGHT } from 'components/Layout/Header/Header.style';
 
 export const HeroWrapper = styled.section`
-  height: calc(100vh - ${HEADER_HEIGHT});
+  display: grid;
+  grid-template-columns: 1fr;
+  position: relative;
+
+  @media screen and ${({ theme }) => theme.screenSizes.lg} {
+    grid-template-columns: repeat(2, 1fr);
+  }
 `;
 
 export const Overlay = styled.div`
@@ -10,17 +16,44 @@ export const Overlay = styled.div`
   inset: 0;
   z-index: 2;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.3);
+  background-color: rgba(0, 0, 0, 0.7);
 `;
 
-export const HeroTextWrapper = styled.section`
+export const HeroActionButtonsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  font-weight: bold;
+  text-transform: uppercase;
+
+  @media screen and ${({ theme }) => theme.screenSizes.md} {
+    flex-direction: row;
+  }
+`;
+
+export const HeroActionButton = styled.a`
+  background-color: ${({ theme }) => theme.colors.red};
+  color: ${({ theme }) => theme.colors.white};
+  width: 130px;
+  height: 50px;
+  padding: 20px;
+  text-align: center;
+  line-height: 100%;
+
+  :hover {
+    color: ${({ theme }) => theme.colors.white};
+  }
+`;
+
+export const HeroTextAndButtonsWrapper = styled.section`
   position: absolute;
   left: 50%;
-  top: 40%;
+  top: calc(${HEADER_HEIGHT} + 5%);
   transform: translate(-50%, -50%);
   width: 100%;
   display: flex;
   flex-direction: column;
+  gap: 20px;
   align-items: center;
   z-index: 3;
 `;
@@ -41,5 +74,5 @@ export const HeroDesc = styled.p`
 
 export const HeroImageWrapper = styled.div`
   position: relative;
-  height: 100%;
+  height: calc(100vh - ${HEADER_HEIGHT});
 `;
