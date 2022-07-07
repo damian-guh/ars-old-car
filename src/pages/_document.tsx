130b839923f3171a67137ac97996363d8138e9e5
ThisGuh <jakiepolska1111@gmail.com>
2022-07-07 12:13:38 +0200

    chore: add gtm

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ import Document, {
   Main,
   NextScript,
 } from 'next/document';
-import Script from 'next/script';
 import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
@@ -45,7 +44,8 @@ class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Mali:wght@400;700&display=swap'
             rel='stylesheet'
           />
-          <Script
+          <script
+            /* eslint-disable-next-line react/no-danger */
             dangerouslySetInnerHTML={{
               __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
