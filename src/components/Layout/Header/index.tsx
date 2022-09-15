43246b2e205d503459021b74505b0a45d816ccd2
damian-guh <jakiepolska1111@gmail.com>
2022-09-15 22:16:15 +0200

    feat: change quick section

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -33,7 +33,9 @@ const Header = () => {
           onClick={() => toggleMenu && toggleMenu((prevState) => !prevState)}
         />
       )}
-      <QuickNewsSection>W piątek (16.09.2022) muzeum nieczynne. Zapraszamy w sobotę</QuickNewsSection>
+      <QuickNewsSection>
+        W piątek (16.09.2022) muzeum nieczynne. Zapraszamy w sobotę
+      </QuickNewsSection>
     </Wrapper>
   );
 };
