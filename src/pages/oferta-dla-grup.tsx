877a0ca9b7bc6884fcb2f5955819e48c83033b03
damian-guh <jakiepolska1111@gmail.com>
2023-03-19 23:30:16 +0100

    feat: change pricing page and improvements on school offer page

--- src/pages/oferta-dla-grup.tsx
+++ src/pages/oferta-dla-grup.tsx
@@ -11,18 +11,28 @@ import OfferImage5 from '../../public/oferta-dla-szkol-5.jpg';
 import OfferImage6 from '../../public/oferta-dla-szkol-6.jpg';
 
 const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  display: grid;
+  grid-template-columns: 1fr;
+  justify-items: center;
   text-align: center;
   font-size: ${({ theme }) => theme.fontSizes.md};
   gap: 15px;
-  padding: 20px;
+  padding: 100px 20px;
   margin: 40px 0;
 
   > * {
     max-width: 1000px;
   }
+
+  @media screen and ${({ theme }) => theme.screenSizes.xl} {
+    grid-template-columns: 1fr 1fr;
+    padding: 20px;
+  }
+`;
+
+const TextWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
 `;
 
 const ImagesWrapper = styled.div`
@@ -31,7 +41,7 @@ const ImagesWrapper = styled.div`
   gap: 15px;
 
   @media screen and ${({ theme }) => theme.screenSizes.lg} {
-    grid-template-columns: repeat(3, 1fr);
+    grid-template-columns: repeat(2, 1fr);
   }
 `;
 
@@ -69,79 +79,86 @@ const OfferForSchoolsPage = () => {
   return (
     <Layout>
       <Wrapper>
-        <h2>Oferta dla szkół i przedszkoli</h2>
-        <div>
+        <TextWrapper>
+          <h2>Oferta dla szkół i przedszkoli</h2>
+          <div>
+            <p>
+              Zapraszamy grupy z przedszkoli i szkół na żywą lekcję motoryzacji
+              na wesoło.
+            </p>
+            <p>Tu historia motoryzacji spotyka się z zabawą 📖🎉🥳✌</p>
+          </div>
+          <div>
+            <p>Zapraszamy do Nas ❤</p>
+          </div>
+          <p>Program wycieczki do ARS OLD CAR obejmuje:</p>
           <p>
-            Zapraszamy grupy z przedszkoli i szkół na żywą lekcję motoryzacji na
-            wesoło.
+            - przejażdżka zabytkowym samochodem strażackim na punkt widokowy, z
+            którego będziemy podziwiać m.in. Zamek Chęciński,
           </p>
-          <p>Tu historia motoryzacji spotyka się z zabawą 📖🎉🥳✌</p>
-        </div>
+          <p>
+            - przejazd do ruin willi Hempla, gdzie zapoznamy się z historią
+            obiektu
+          </p>
+          <p>
+            - zwiedzanie największej w Polsce wystawy poświęconej samochodowi
+            znanego z filmu Żandarm z Saint Tropez, kultowego Citroena 2CV
+          </p>
+          <p>- konkursy motoryzacyjne QR dla dzieci</p>
+          <p>- prezentacje multimedialne &quot;wokół motoryzacji&quot;</p>
+          <p>- gry i zabawy na &quot;świeżym powietrzu&quot;</p>
+          <p>- czas wolny</p>
+          <p>- ognisko</p>
+          <p>
+            - posiłek (kiełbaska z grilla, pieczone ziemniaki , pieczywo, sosy,
+            herbata)
+          </p>
+          <p>- możliwość zamówienia cateringu</p>
+          <p>
+            Czas trwania wycieczki około 3 godzin. Koszt zwiedzania z posiłkiem
+            to 35 zł od osoby (opiekunowie nie płacą), bez posiłku 20 zł. Każdy
+            uczestnik wycieczki dostanie pamiątkową naklejkę samochodu 2CV.
+          </p>
+          <p>Na miejscu do nabycia tematyczne pamiątki !</p>
+          <p>
+            Parking dla autokaru, dojazd bezpośrednio pod wejście do muzeum.
+          </p>
+          <b>Dodatkowa atrakcja - świat wirtualny VR ( nowość w 2023 !!! )</b>
+          <b>Tylko 7 minut od Chęcin.</b>
+          <b>Muzeum otwarte w 2023 r. w okresie 15 kwiecień - 20 październik</b>
+          <b>
+            W przypadku wycieczek kilkudniowych służymy pomocą w organizacji
+            trasy pobytu, doboru atrakcji turystycznych, noclegów oraz
+            cateringu.
+          </b>
+          <p>ZAPRASZAMY‼️❤</p>
+          <a href='mailto:kontakt@arsoldcar.pl'>
+            📧 e-mail: kontakt@arsoldcar.pl
+          </a>
+          <a href='tel:+48 515 355 533'>☎️ tel: +48 515 355 533</a>
+        </TextWrapper>
         <div>
-          <p>Zapraszamy do Nas ❤</p>
+          <ImagesWrapper>
+            {images.map(({ asset }) => (
+              <ImageWrapper key={asset[0].id}>
+                <Image
+                  src={asset[0].url}
+                  layout='fill'
+                  quality={95}
+                  objectFit='cover'
+                  onClick={(event) => handleModal(event)}
+                />
+              </ImageWrapper>
+            ))}
+          </ImagesWrapper>
+          <ImageModal
+            isModalOpen={isModalOpen}
+            setModalOpen={setModalOpen}
+            clickedImage={clickedImage}
+            // @ts-ignore
+            allImages={images}
+          />
         </div>
-        <p>Program wycieczki do ARS OLD CAR obejmuje:</p>
-        <p>
-          - przejażdżka zabytkowym samochodem strażackim na punkt widokowy, z
-          którego będziemy podziwiać m.in. Zamek Chęciński,
-        </p>
-        <p>
-          - przejazd do ruin willi Hempla, gdzie zapoznamy się z historią
-          obiektu
-        </p>
-        <p>
-          - zwiedzanie największej w Polsce wystawy poświęconej samochodowi
-          znanego z filmu Żandarm z Saint Tropez, kultowego Citroena 2CV
-        </p>
-        <p>- konkursy motoryzacyjne QR dla dzieci</p>
-        <p>- prezentacje multimedialne &quot;wokół motoryzacji&quot;</p>
-        <p>- gry i zabawy na &quot;świeżym powietrzu&quot;</p>
-        <p>- czas wolny</p>
-        <p>- ognisko</p>
-        <p>
-          - posiłek (kiełbaska z grilla, pieczone ziemniaki , pieczywo, sosy,
-          herbata)
-        </p>
-        <p>- możliwość zamówienia cateringu</p>
-        <p>
-          Czas trwania wycieczki około 3 godzin. Koszt zwiedzania z posiłkiem to
-          35 zł od osoby (opiekunowie nie płacą), bez posiłku 20 zł. Każdy
-          uczestnik wycieczki dostanie pamiątkową naklejkę samochodu 2CV.
-        </p>
-        <p>Na miejscu do nabycia tematyczne pamiątki !</p>
-        <p>Parking dla autokaru , dojazd bezpośrednio pod wejście do muzeum.</p>
-        <b>Dodatkowa atrakcja - świat wirtualny VR ( nowość w 2023 !!! )</b>
-        <b>Tylko 7 minut od Chęcin.</b>
-        <b>Muzeum otwarte w 2023 r. w okresie 15 kwiecień - 20 październik</b>
-        <b>
-          W przypadku wycieczek kilkudniowych służymy pomocą w organizacji trasy
-          pobytu, doboru atrakcji turystycznych, noclegów oraz cateringu.
-        </b>
-        <p>ZAPRASZAMY‼️❤</p>
-        <a href='mailto:kontakt@arsoldcar.pl'>
-          📧 e-mail: kontakt@arsoldcar.pl
-        </a>
-        <a href='tel:+48 515 355 533'>☎️ tel: +48 515 355 533</a>
-        <ImagesWrapper>
-          {images.map(({ asset }) => (
-            <ImageWrapper key={asset[0].id}>
-              <Image
-                src={asset[0].url}
-                layout='fill'
-                quality={95}
-                objectFit='cover'
-                onClick={(event) => handleModal(event)}
-              />
-            </ImageWrapper>
-          ))}
-        </ImagesWrapper>
-        <ImageModal
-          isModalOpen={isModalOpen}
-          setModalOpen={setModalOpen}
-          clickedImage={clickedImage}
-          // @ts-ignore
-          allImages={images}
-        />
       </Wrapper>
     </Layout>
   );
