068f6e0806da7a72ea0b70e6a16f692dfa68892f
damian-guh <jakiepolska1111@gmail.com>
2025-04-06 20:40:03 +0200

    feat: update reservation page without form

--- src/pages/rezerwacje.tsx
+++ src/pages/rezerwacje.tsx
@@ -15,33 +15,31 @@ import { MAIL, PHONE } from 'utils/constants';
 const mailHref = `mailto:${MAIL}`;
 const phoneHref = `tel:${PHONE}`;
 
-const ReservationPage: NextPage = () => {
-  return (
-    <Layout>
-      <Wrapper>
-        <FormSectionWrapper>
-          <StyledHeading>Zarezerwuj termin już teraz!</StyledHeading>
-          <StyledInfoP>
-            Rezerwacja możliwa jest telefonicznie lub mailowo.
-          </StyledInfoP>
-          <StyledInfoP>
-            Podaj liczbę osób, interesujący Cię termin najpóźniej 1 dzień przed
-            nim oraz ewentualne szczegóły.
-          </StyledInfoP>
-          <StyledInfoP>
-            E-mail: <a href={mailHref}>{MAIL}</a>
-          </StyledInfoP>
-          <StyledInfoP>
-            Telefon: <a href={phoneHref}>{PHONE}</a>
-          </StyledInfoP>
-        </FormSectionWrapper>
-        <FlippingCardSection>
-          <FirefighterTruckAttraction />
-          <FlippingCard content={FLIPPING_CARD_COMPANY_EVENTS} />
-        </FlippingCardSection>
-      </Wrapper>
-    </Layout>
-  );
-};
+const ReservationPage: NextPage = () => (
+  <Layout>
+    <Wrapper>
+      <FormSectionWrapper>
+        <StyledHeading>Zarezerwuj termin już teraz!</StyledHeading>
+        <StyledInfoP>
+          Rezerwacja możliwa jest telefonicznie lub mailowo.
+        </StyledInfoP>
+        <StyledInfoP>
+          Podaj liczbę osób, interesujący Cię termin najpóźniej 1 dzień przed
+          nim oraz ewentualne szczegóły.
+        </StyledInfoP>
+        <StyledInfoP>
+          E-mail: <a href={mailHref}>{MAIL}</a>
+        </StyledInfoP>
+        <StyledInfoP>
+          Telefon: <a href={phoneHref}>{PHONE}</a>
+        </StyledInfoP>
+      </FormSectionWrapper>
+      <FlippingCardSection>
+        <FirefighterTruckAttraction />
+        <FlippingCard content={FLIPPING_CARD_COMPANY_EVENTS} />
+      </FlippingCardSection>
+    </Wrapper>
+  </Layout>
+);
 
 export default ReservationPage;
